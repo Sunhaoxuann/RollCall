4763e2aa9fed002b898a11951a9ff3b935ed3ade
= <=>
2024-06-09 22:15:00 +0800

    优化
    *1算法性能优化
    *2名字居中（bug修复）
    *3设置按钮点击判断

--- RollCall.py
+++ RollCall.py
@@ -23,17 +23,22 @@ def textshow(text,rgb = (0,0,0),siz = 72,ttf = '华文楷体'):#渲染文字
 
 mode = 'main'
 showtext_mainwindows = '随机点名'
-namelist = ['叶杨阳','肖逸凡','王沐恩','黄皓轩','项涵哲','邹帆','闻张依','翁睿毅','李御轩','张峰铭','韦思桐','沈凯昕','林浩','吴珈栩','袁晓晴','于妙盈','俞昭阳','申屠绍越','姚智宇','陶奕畅','孙昊轩','高智杰','严天浩','李嘉怡','何相南','黄圣曦','周炜昊','吴文萱','李乐山','殷一铭','蒋一凡','孙周哲翊','沈雨涵','程天宇','郑杰','蒋哲聪','徐书亚','林馨雯','方浩如','刘翔','边峻熙','叶俊浩','张可维','李宏振','高浩哲','产洲俊','王博辉','陈沈捷']
-t = 0
+namelist = ['叶杨阳','肖逸凡','王沐恩','黄皓轩','项涵哲','邹帆','闻张依','翁睿毅','李御轩','张峰铭','韦思桐','沈凯昕','林浩','吴珈栩','袁晓晴','于妙盈','俞昭阳','申屠绍越','姚智宇','陶奕畅',
+            '孙昊轩','高智杰','严天浩','李嘉怡','何相南','黄圣曦','周炜昊','吴文萱','李乐山','殷一铭','蒋一凡','孙周哲翊','沈雨涵','程天宇','郑杰','蒋哲聪','徐书亚','林馨雯','方浩如','刘翔',
+            '边峻熙','叶俊浩','张可维','李宏振','高浩哲','产洲俊','王博辉','陈沈捷']
 rolltime = 20
-ifread = True
+ifread = False
 color = (0,0,0)
+ifclick = True
+ifchoice = False
+times = 0
+rollspeed = 20#每秒 
 
 pg.init()#初始化
 windows = pg.display.set_mode((385,141))
 pg.display.set_caption('RollCall')
 mode1winndows(windows)
-windows.blit(textshow('随机点名',color),(0,25))
+windows.blit(textshow('随机点名',color),(46,19))
 
 while True:
     events = pg.event.get()#获取事件
@@ -45,25 +50,36 @@ while True:
     
     if mode == 'main':#进入主窗口
         for enevt in events:
-            if enevt.type == pg.MOUSEBUTTONDOWN and enevt.button == 1:#判断左键点击
-                print('鼠标左键点击')
-                t = 1
+            if enevt.type == pg.MOUSEBUTTONDOWN and enevt.button == 1 and  event.pos[0] >= 354 and 31 >= event.pos[1]:#判断左键点击
+                mode = 'login'
+                ifclick = True
+                times = 1
+                ifchoice = False
                 break
-        if t == 1:
-            for i in range(rolltime):#滚动动效
-                mode1winndows(windows)
-                showtext_mainwindows = random.choice(namelist)
-                windows.blit(textshow(showtext_mainwindows,color),[0,25])
-                pg.display.update()
-                sleep(0.05)
-            if ifread == True:
-                langdu(showtext_mainwindows)
-            t = 0
+
+            elif enevt.type == pg.MOUSEBUTTONDOWN and enevt.button == 1 and ifclick == True:#判断左键点击
+                ifchoice = True
+                break
 
     elif mode == 'login':#进入登录窗口
-        mode3windows()
+        mode3windows(windows)
 
     elif mode == 'setting':#进入设置窗口
-        mode2windows()
+        mode2windows(windows)
     
+    if ifchoice == True:
+        times += 1
+        mode1winndows(windows)
+        showtext_mainwindows = random.choice(namelist)
+        windows.blit(textshow(showtext_mainwindows,color),(46 + (4 - len(showtext_mainwindows)) * 73.25 / 2, 25))
+        if times == rolltime:
+            ifread = True
+            times = 1
+            ifchoice = False
+        sleep(1/rollspeed)
+
     pg.display.update()
+
+    if ifread == True:
+        langdu(showtext_mainwindows)
+        ifread = False
