391f68ef4706b78656ae62e0ac5824dc2f855965
sunhaoxuan <shx_203172525@outlook.com>
2024-12-28 16:54:16 +0800

    save files
    just save files

--- RollCall.py
+++ RollCall.py
@@ -7,6 +7,8 @@ import pyttsx3
 from time import sleep
 import threading
 import base64
+import pandas as pd
+import sys
 
 def mode1windows(screen): # 主窗口基本布局
     screen.fill((240, 240, 240))
@@ -16,6 +18,7 @@ def mode1windows(screen): # 主窗口基本布局
 
 def mode2windows(screen): # 设置窗口基本布局
     screen.fill((240, 240, 240))
+    screen.blit(textshow('←返回', (113, 96, 232), 15), (30, 100))
 
 def mode3windows(screen): # 登录窗口基本布局
     screen.fill((240, 240, 240))
@@ -38,25 +41,10 @@ def mode3windows(screen): # 登录窗口基本布局
     # screen.blit(textshow('1 2 3 4 5 6', black, 21), (33, 60))
     # pg.draw.rect(screen, (225, 225, 225), (25, 50, 335, 25), 0)
     # pg.draw.rect(screen, (220, 220, 220), (290, 90, 70, 25), 0)
-    
-def mode3windows2(screen): # 登录窗口基本布局2
-    screen.fill((240, 240, 240))
-    screen.blit(textshow('请输入密码', black, 20), (25, 15))
-    screen.blit(textshow('_ _ _ _ _ _', black, 20), (33, 60))
-    screen.blit(textshow('1', black, 30), (210,14))
-    screen.blit(textshow('4', black, 30), (210,54))
-    screen.blit(textshow('7', black, 30), (210,94))
-    screen.blit(textshow('2', black, 30), (250,14))
-    screen.blit(textshow('5', black, 30), (250,54))
-    screen.blit(textshow('8', black, 30), (250,94))
-    screen.blit(textshow('3', black, 30), (290,14))
-    screen.blit(textshow('6', black, 30), (290,54))
-    screen.blit(textshow('9', black, 30), (290,94))
-    screen.blit(textshow('0', black, 30), (330,54))
-    screen.blit(textshow('确定', (113, 96, 232), 20), (320, 100))
-    screen.blit(textshow('删除', (113, 96, 232), 20), (320, 20))
-    screen.blit(textshow('←返回', (113, 96, 232), 15), (30, 100))
 
+def messagewindows(screen, text, rgb=(0, 0, 0), siz=72, ttf='华文楷体'): # 信息窗口
+    screen.fill((240, 240, 240))
+    screen.blit(textshow(text, rgb, siz, ttf), (25, 50))
 
 def langdu(text): # 朗读函数
     engine = pyttsx3.init()
@@ -86,6 +74,13 @@ def readbinfile(file): # 读取文件函数
     password = decoding(password)
     return eval(namelist), password
 
+def writebinfile(file, namelist, password): # 写入文件函数
+    namelist = encodeing(str(namelist))
+    password = encodeing(str(password))
+    with open(file, 'w') as f:
+        f.write(namelist)
+        f.write(password)
+
 def _var(vars):
     print(f"value:{vars}, type:{type(vars)}, len:{len(vars)}")
 
@@ -106,7 +101,7 @@ ifchoice = False
 times = 0
 rollspeed = 20  # 每秒
 thread = None
-namelist, password = readbinfile('.\\PythonProgram\\RollCall\\config.bin')
+namelist, password = readbinfile('.\\config.bin')
 n = 0
 
 pg.init()  # 初始化
@@ -115,10 +110,21 @@ pg.display.set_caption('RollCall')
 
 
 while True:
+
+    
+    #debug!!!---------------------------------------------------------------------------------------------------
+    #-------------------------------------------------------------------------------------------------------------
+    #-------------------------------------------------------------------------------------------------------------
+    #-------------------------------------------------------------------------------------------------------------
+    mode = 'setting'
+
     if mode == 'main':
         while True:
             # 运算
             
+            if mode != 'main':
+                break
+
             # 获取事件
             events = pg.event.get()  
             
@@ -273,8 +279,27 @@ while True:
             for event in events:
                 if event.type == pg.QUIT:
                     os._exit(0)
+
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                
+                if event.pos[0] in range(29, 76) and event.pos[1] in range(99, 121): 
+                   mode = 'main'
+                   event = nothing()
+                   break
+
+
              # 渲染
-            mode1windows(windows)
+            mode2windows(windows)
+            
+            windows.blit(textshow('导入名单', (0, 0, 0), 15), (5, 5))
+            pg.draw.rect(windows, (0, 0, 0), (5, 5, 60, 20), 1)
+
+            windows.blit(textshow('生成名单导入模板', (0, 0, 0), 15), (40, 30))
+            pg.draw.rect(windows, (0, 0, 0), (40, 30, 121, 20), 1)
+
+            windows.blit(textshow('┕', (0, 0, 0), 15), (10, 30))
+
+            pg.display.update()
             
 # HISTORY CODE
 ''' 
